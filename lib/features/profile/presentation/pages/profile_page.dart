4a5b896da8efdc874f2b86adb02d113a87373ac9
Kim JaeHyung <you3192001@icloud.com>
2026-01-27 17:41:05 +0900

    [Feat] Follow 등의 정보 섹션 및 CustomScrollView 변경

--- lib/features/profile/presentation/pages/profile_page.dart
+++ lib/features/profile/presentation/pages/profile_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:no_ai_sns/design_system/tokens/spacing.dart';
+import 'package:no_ai_sns/features/profile/presentation/sub_widgets/w_profile_follow_info_section.dart';
 import 'package:no_ai_sns/features/profile/presentation/sub_widgets/w_profile_top_section.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
@@ -8,16 +11,35 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Text('Profile'),
-            ProfileTopSectionWidget(
-              profileUrlString:
-                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTx2Z4LcJOPwaTwwqZ-qIxXxKicRR6sa-Ge6A&s",
-              userName: 'Alex Rivera',
-              userEmail: 'alex.rivera@example.com',
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              centerTitle: false,
+              title: 'My Profile'.text.size(20).make(),
+              backgroundColor: context.theme.scaffoldBackgroundColor,
+              elevation: 0,
+              shadowColor: Colors.transparent,
+              surfaceTintColor: Colors.transparent,
+              scrolledUnderElevation: 0,
+            ),
+
+            SliverToBoxAdapter(
+              child: ProfileTopSectionWidget(
+                profileUrlString:
+                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTx2Z4LcJOPwaTwwqZ-qIxXxKicRR6sa-Ge6A&s",
+                userName: 'Alex Rivera',
+                userEmail: 'alex.rivera@example.com',
+              ),
+            ),
+
+            SliverToBoxAdapter(
+              child: ProfileFollowInfoSectionWidget(
+                followers: '12.3k',
+                following: '456',
+                postCount: '789',
+              ).pSymmetric(v: AppSpacing.xl).pSymmetric(h: AppSpacing.lg),
             ),
           ],
         ),
